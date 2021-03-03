e4281891e5a6968a42e39e406f84dddacc048534
Jennifer Fitzhugh <74557543+jfitzu@users.noreply.github.com>
2021-03-03 16:13:36 -0700

    Revert "Prevent NPE error in Pojo's with synthetic properties"
    This reverts commit 9069d7ab6e5eedb716a3e0ad88f2cb7aae6d9960.

--- bson/src/main/org/bson/codecs/pojo/PojoBuilderHelper.java
+++ bson/src/main/org/bson/codecs/pojo/PojoBuilderHelper.java
@@ -166,8 +166,6 @@ final class PojoBuilderHelper {
     }
 
     private static boolean isAssignableClass(final Class<?> propertyTypeClass, final Class<?> typeDataClass) {
-        notNull("propertyTypeClass", propertyTypeClass);
-        notNull("typeDataClass", typeDataClass);
         return propertyTypeClass.isAssignableFrom(typeDataClass) || typeDataClass.isAssignableFrom(propertyTypeClass);
     }
 
