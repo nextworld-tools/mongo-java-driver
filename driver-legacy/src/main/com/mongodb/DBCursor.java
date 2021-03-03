f3a37ab3ebf3375d6b6697878cbb6028e93855d2
Jennifer Fitzhugh <74557543+jfitzu@users.noreply.github.com>
2021-03-03 16:13:36 -0700

    Revert "Add Javadoc indicating which server version supports explain"
    This reverts commit cd2231dc9cc44cccd4a4a14c51ae649270c6cc59.

--- driver-legacy/src/main/com/mongodb/DBCursor.java
+++ driver-legacy/src/main/com/mongodb/DBCursor.java
@@ -335,7 +335,7 @@ public class DBCursor implements Cursor, Iterable<DBObject> {
      * @return a {@code DBObject} containing the explain output for this DBCursor's query
      * @throws MongoException if the operation failed
      * @mongodb.driver.manual reference/command/explain Explain Output
-     * @mongodb.server.release 3.0
+     * @deprecated Replace with direct use of the explain command using the runCommand helper method
      */
     @Deprecated
     public DBObject explain() {
